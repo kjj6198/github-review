e29128b02973d54a35cb9f52bda6e36bdf280ed9
kjj6198 <kjj6198@gmail.com>
2020-04-26 17:10:01 +0900

    add language util

--- src/utils.ts
+++ src/utils.ts
@@ -15,3 +15,45 @@ export function findMinMax(nums: number[]) {
     min: Math.min(...nums),
   };
 }
+
+type AvailableLangs = "zh-TW" | "en" | "ja" | "jp";
+export function getUserLanguage(
+  language: string,
+  languages: ReadonlyArray<string>
+): AvailableLangs {
+  const availableLang: Array<string> = ["zh-TW", "en", "ja", "jp"];
+  if (availableLang.includes(language)) {
+    return language as AvailableLangs;
+  } else if (languages.length) {
+    return (availableLang.includes(languages[0])
+      ? languages[0]
+      : "en") as AvailableLangs;
+  }
+
+  return "en";
+}
+
+export function createTranslation(lang: string, translation: any) {
+  return function translationFn(
+    key: string,
+    ...interpolations: unknown[]
+  ): string {
+    // only happy path
+    const parts = key.split(".");
+    const string = parts.reduce((acc, key) => {
+      return acc[key];
+    }, translation);
+
+    if (interpolations.length) {
+      return string.replace(/\{(\d+)\}/g, function matchFn(
+        match: string,
+        group: string
+      ) {
+        const num = parseInt(group, 10);
+        return interpolations[num];
+      });
+    }
+
+    return string;
+  };
+}
