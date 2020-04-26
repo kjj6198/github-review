1d2e99913005f4606560d37579af7f34d002a5da
kjj6198 <kjj6198@gmail.com>
2020-04-26 17:08:24 +0900

    add jp, zh-TW, en translation

---
+++ src/config/translation.ts
@@ -0,0 +1,144 @@
+const translation = {
+  "zh-TW": {
+    long_time: {
+      name: "🧐 搾時 Pull Request",
+      description: "超過 {0} 天以上未 merged",
+    },
+    contribution: {
+      name: "👍 貢獻",
+      description: "貢獻的程式碼總數（增加及刪除）",
+    },
+    commits: {
+      name: "Commit 數",
+      description: "Commit 的數量",
+    },
+    created: {
+      name: "👍 提交 PR 總數",
+      description: "提交的 PR 總數，只計算已 merged",
+    },
+    comments: {
+      name: "留言數",
+      description: "每個自己提交的 PR 中的留言數",
+    },
+    reviewed: {
+      name: "👍 Reviewed PR",
+      description:
+        "Reviewed 過的 PR 總數（包含 approve, comment, requested change)",
+    },
+    requested: {
+      name: "👎 無回應的 Pull Request 數",
+      description: "別人在 Pull Request 當中要求 Review，但你沒有回應",
+    },
+    criticizing: {
+      name: "🙌 有爭議的 Pull Request",
+      description: "Comment 數大於 10",
+    },
+  },
+  ja: {
+    long_time: {
+      name: "🧐 長時間プルリク",
+      description: "三日間以上過ぎてマージされていない",
+    },
+    contribution: {
+      name: "👍 Contribution",
+      description: "マージされてコードの合計",
+    },
+    commits: {
+      name: "コミット数",
+      description: "マージされたプルリクのコミット数",
+    },
+    created: {
+      name: "👍 プルリク合計",
+      description: "自分が作ったプルリク（マージ済）",
+    },
+    comments: {
+      name: "コメント數",
+      description: "マージされたプルリクのコメント数",
+    },
+    reviewed: {
+      name: "👍 レビュー済プルリク",
+      description:
+        "レビュー済のプルリク（approve, comment, requested changeも含む）",
+    },
+    requested: {
+      name: "👎 レビューしてないプルリク数",
+      description: "プルリクでレビューお願いされたが、返事がしてなかった",
+    },
+    criticizing: {
+      name: "🙌 議論プルリク",
+      description: "プルリクのコメント数は10超えた",
+    },
+  },
+  jp: {
+    long_time: {
+      name: "🧐 長時間プルリク",
+      description: "三日間以上過ぎてマージされていない",
+    },
+    contribution: {
+      name: "👍 Contribution",
+      description: "マージされてコードの合計",
+    },
+    commits: {
+      name: "コミット数",
+      description: "マージされたプルリクのコミット数",
+    },
+    created: {
+      name: "👍 プルリク合計",
+      description: "自分が作ったプルリク（マージ済）",
+    },
+    comments: {
+      name: "コメント數",
+      description: "マージされたプルリクのコメント数",
+    },
+    reviewed: {
+      name: "👍 レビュー済プルリク",
+      description:
+        "レビュー済のプルリク（approve, comment, requested changeも含む）",
+    },
+    requested: {
+      name: "👎 レビューしてないプルリク数",
+      description: "プルリクでレビューお願いされたが、返事がしてなかった",
+    },
+    criticizing: {
+      name: "🙌 議論プルリク",
+      description: "プルリクのコメント数は10超えた",
+    },
+  },
+  en: {
+    long_time: {
+      name: "🧐 Long Time Pull Request",
+      description: "Pull Reqeust that merged after created more than 3 days",
+    },
+    contribution: {
+      name: "👍 Contribution",
+      description: "addition and deletion for merged pull reqeust",
+    },
+    commits: {
+      name: "Commit count",
+      description: "Commit count for merged pull request",
+    },
+    created: {
+      name: "👍 Created Pull Request",
+      description: "Pull Request created by you and got merged",
+    },
+    comments: {
+      name: "Comments",
+      description: "Sum of total comment for your pull request",
+    },
+    reviewed: {
+      name: "👍 Reviewed PR",
+      description:
+        "PR that you've reviewed (including approve, requested-change, comment)",
+    },
+    requested: {
+      name: "👎 Requested Pull Request",
+      description: "Someone requested you to review, but you didn't respond",
+    },
+    criticizing: {
+      name: "🙌 Controversal Pull Request",
+      description: "Pull Request comment more than 10",
+    },
+  },
+};
+
+export default translation;
