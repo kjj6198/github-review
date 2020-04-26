3ffd685f623e8e4e082ce017fc675120bb2fa9c8
kjj6198 <kjj6198@gmail.com>
2020-04-26 11:07:28 +0900

    extract query into another file

---
+++ src/queries.ts
@@ -0,0 +1,76 @@
+import { gql } from "apollo-boost";
+
+export const userFields = gql`
+  fragment GithubUserField on User {
+    id
+    name
+    bio
+    login
+    avatarUrl
+    followers {
+      totalCount
+    }
+    following {
+      totalCount
+    }
+    pullRequests {
+      totalCount
+    }
+  }
+`;
+
+export const GITHUB_USER_QUERY = gql`
+  ${userFields}
+  query searchGithubUser($query: String!, $after: String) {
+    search(type: USER, query: $query, after: $after, first: 5) {
+      edges {
+        node {
+          ...GithubUserField
+        }
+      }
+    }
+  }
+`;
+
+export const COUNT_QUERY = gql`
+  query CountQuery($query: String!) {
+    search(type: ISSUE, first: 100, query: $query) {
+      issueCount
+    }
+  }
+`;
+
+export const PULL_REQUESTS_QUERY = gql`
+  query GetPullRequestsByQuery($query: String!, $after: String) {
+    search(type: ISSUE, first: 100, query: $query, after: $after) {
+      issueCount
+      edges {
+        node {
+          ... on PullRequest {
+            id
+            title
+            createdAt
+            mergedAt
+            deletions
+            additions
+            merged
+            milestone {
+              id
+              title
+            }
+            comments {
+              totalCount
+            }
+            commits {
+              totalCount
+            }
+          }
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }
+`;
